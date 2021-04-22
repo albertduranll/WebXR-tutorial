9e1c4f4164fd350296e4716e221028f29ba8625c
albertduranll <alduli@floridauniversitaria.es>
2021-04-23 00:24:57 +0200

    fff

--- complete/lecture2_6/app.js
+++ complete/lecture2_6/app.js
@@ -84,6 +84,7 @@ class App{
                 //self.chair.position.set(0, -10, 0);
                 gltf.scene.position.set(0, 1, 0);
                 //gltf.scene.rotation.set(new THREE.Vector3( Math.PI / 2, 0, 0));
+                console.log("Posició capturada:");
                 console.log(gltf.scene.position.x);
                 console.log(gltf.scene.position.y);
                 console.log(gltf.scene.position.z);
